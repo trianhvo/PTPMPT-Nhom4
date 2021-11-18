3c475182deba46bc890ad5d0c63e5b0f9ac213d4
Vien Quoc Anh <quocanhvien99@gmail.com>
2021-11-18 16:42:21 +0700

    Cài đặt expressjs + các thư viện + thiết kế model user

---
+++ app.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const ExpressError = require('./utils/ExpressError');
+const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const MongoStore = require('connect-mongo');
+const mongoSanitize = require('express-mongo-sanitize');
+
+const User = require('./models/user');
+
+require('dotenv').config();
+
+const dbUrl = process.env.DB_URL;
+
+const userRoutes = require('./routes/users');
+
+mongoose.connect(dbUrl);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+	console.log('Database connected');
+});
+
+const app = express();
+
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+	mongoSanitize({
+		replaceWith: '_',
+	})
+);
+
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
+
+const options = {
+	mongoUrl: dbUrl,
+	secret,
+	touchAfter: 24 * 3600,
+};
+const sessionConfig = {
+	store: MongoStore.create(options),
+	name: 'session',
+	secret,
+	resave: false,
+	saveUninitialized: true,
+	cookie: {
+		httpOnly: true,
+		// secure: true,
+		expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+		maxAge: 1000 * 60 * 60 * 24 * 7,
+	},
+};
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+	res.locals.currentUser = req.user;
+	res.locals.success = req.flash('success');
+	res.locals.error = req.flash('error');
+	next();
+});
+
+app.use('/', userRoutes);
+
+app.get('/', (req, res) => {
+	res.render('home');
+});
+
+app.all('*', (req, res, next) => {
+	next(new ExpressError('Page Not Found', 404));
+});
+
+app.use((err, req, res, next) => {
+	const { statusCode = 500 } = err;
+	if (!err.message) err.message = 'Oh No, Something Went Wrong!';
+	res.status(statusCode).render('error', { err });
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+	console.log(`Serving on port ${port}`);
+});
