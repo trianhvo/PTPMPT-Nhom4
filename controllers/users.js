12c3825c6a5d101f1378211c5cd0dab5059de605
trianhvo <trianhvo99@gmail.com>
2021-11-18 23:58:01 +0700

    update controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,30 @@
 const User = require('../models/user');
 
+module.exports.renderRegister = (req, res) => {
+	res.render('users/register');
+};
+
+module.exports.register = async (req, res, next) => {
+	try {
+		const { firstName, lastName, email, username, password } = req.body;
+		const user = new User({ firstName, lastName, email, username });
+		const registeredUser = await User.register(user, password);
+		console.log(registeredUser);
+		req.login(registeredUser, (err) => {
+			if (err) return next(err);
+			req.flash('success', 'Welcome to ImageShare!');
+			res.redirect('/pictures');
+		});
+	} catch (e) {
+		req.flash('error', e.message);
+		res.redirect('register');
+	}
+};
+
+module.exports.renderLogin = (req, res) => {
+	res.render('users/login');
+};
+
 module.exports.renderLogin = (req, res) => {
     res.render('users/login');
 };
